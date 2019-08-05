4f6c9a4d5388e122293bb08c99714497c369d131
traktorist3872548 <redisav@gmail.com>
2019-08-06 00:15:28 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 export VAULT_ADDR='https://localhost:8200'
-export VAULT_CAPATH=/opt/vault/ca/certs/ca.cert.pem
 /opt/vault/vault operator init -key-shares=1 -key-threshold=1 | tee /etc/vault/init.file
