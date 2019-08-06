241f6ac2c85f3f36d7ade5d4491eb41bc7bc999c
root <root@vaultserver.some.domain>
2019-08-05 20:38:50 -0400

    modified:   poststart.sh 	modified:   uninstall.sh

--- poststart.sh
+++ poststart.sh
@@ -2,11 +2,13 @@
 export VAULT_ADDR='https://localhost:8200'
 
 if [[ ! -f /etc/vault/init.file ]] ; then
+    echo 'do one' 
     /opt/vault/vault operator init -key-shares=1 -key-threshold=1 | tee /etc/vault/init.file
+    echo 'do three'
 fi
 
-#export root token to env var
+echo 'export root token to env var'
 export VAULT_TOKEN=$(egrep -m5 '^Initial Root Token' /etc/vault/init.file | cut -f2- -d: | tr -d ' ')
 
-#unseals the vault:
+echo 'unseals the vault'
 egrep -m3 '^Unseal Key' /etc/vault/init.file | cut -f2- -d: | tr -d ' ' | while read key; do   /opt/vault/vault operator unseal ${key}; done
--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,7 @@ systemctl daemon-reload
 rm -rf /etc/vault/
 rm -rf /opt/vault/
 userdel vault
+
+rm -rf download
+rm -rf vault
+rm -rf work
