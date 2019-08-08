1116370b880ab8f7f1c7331fa31546e1ebc5f13a
traktorist3872548 <redisav@gmail.com>
2019-08-08 13:28:32 +0300

    Update poststart.sh

--- poststart.sh
+++ poststart.sh
@@ -15,5 +15,8 @@ fi
 echo 'export root token to env var'
 export VAULT_TOKEN=$(egrep -m5 '^Initial Root Token' /etc/vault/init.file | cut -f2- -d: | tr -d ' ')
 
+#add env var via systemd override
+echo Environment=VAULT_TOKEN=${VAULT_TOKEN} >> /etc/systemd/system/vault.d/override.conf
+
 echo 'unseals the vault'
 egrep -m3 '^Unseal Key' /etc/vault/init.file | cut -f2- -d: | tr -d ' ' | while read key; do   /opt/vault/vault operator unseal ${key}; done
