c66d0eeaf1dd7588472e32ba0b8abfe4513d1536
traktorist3872548 <redisav@gmail.com>
2019-08-23 13:59:46 +0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 yum install unzip git java-1.8.0-openjdk java-1.8.0-openjdk-devel -y
+# Install fpm on centos
+yum install ruby-devel gcc make rpm-build rubygems -y
+gem install --no-ri --no-rdoc fpm
+export PATH=$PATH:/usr/local/bin
+fpm --version
