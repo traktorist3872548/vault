ed0bbecf364cf3d4c3c90d5e5b7dfa0904bb026e
traktorist3872548 <redisav@gmail.com>
2019-08-26 16:23:12 -0400

    modified:   INSTALL.sh 	modified:   README.md 	modified:   pack-inst-scrpt/after_install.sh 	modified:   pack-inst-scrpt/after_remove.sh 	modified:   unseal.sh 	modified:   vault.hcl 	modified:   vault.service

--- pack-inst-scrpt/after_install.sh
+++ pack-inst-scrpt/after_install.sh
@@ -1,16 +1,18 @@
+
 #!/bin/bash -x
 export VAULT_ADDR='http://0.0.0.0:8200'
-chmod -R 770 /opt/vault
+chmod -R 777 /opt/vault
 chown -R vault:vault /opt/vault
 mkdir -p /etc/systemd/system/vault.service.d
 echo [Service] > /etc/systemd/system/vault.service.d/override.conf
 chown vault:vault /etc/systemd/system/vault.service.d/override.conf
-chmod 770 /etc/systemd/system/vault.service.d/override.conf
+chmod 777 /etc/systemd/system/vault.service.d/override.conf
 echo Environment=VAULT_ADDR='http://0.0.0.0:8200' >> /etc/systemd/system/vault.service.d/override.conf
 nohup /opt/vault/vault server -config=/opt/vault/vault.hcl &
 /opt/vault/vault operator init -key-shares=1 -key-threshold=1 | tee /opt/vault/init.file
 export VAULT_TOKEN=$(egrep -m5 '^Initial Root Token' /opt/vault/init.file | cut -f2- -d: | tr -d ' ')
 echo Environment=VAULT_TOKEN=${VAULT_TOKEN} >> /etc/systemd/system/vault.service.d/override.conf
-chmod -R 770 /opt/vault
+pkill vault
+chmod -R 777 /opt/vault
 chown -R vault:vault /opt/vault
 systemctl daemon-reload
--- pack-inst-scrpt/after_remove.sh
+++ pack-inst-scrpt/after_remove.sh
@@ -1,18 +1,7 @@
 #!/bin/bash -x
 systemctl stop vault
 /usr/sbin/userdel vault
-REDHAT_VAULT_SRV='/usr/lib/systemd/system/vault.service'
-REDHAT_VAULT_SRV_OVERRIDE='/etc/systemd/system/vault.service.d/'
-DEBIAN_VAULT_SRV='/lib/systemd/system/vault.service'
-INSTALL_PATH='/opt/vault'
-
-if [ -f $REDHAT_VAULT_SRV ]; then
-   rm -rf $REDHAT_VAULT_SRV $INSTALL_PATH $REDHAT_VAULT_SRV_OVERRIDE
-   systemctl daemon-reload
-elif [ $DEBIAN_VAULT_SRV ]; then
-   rm -rf $DEBIAN_VAULT_SRV $INSTALL_PATH
-   systemctl daemon-reload
-else
-   echo 'vault.service does not exist'
-fi
+rm -rf /usr/lib/systemd/system/vault.service
+rm -fr /etc/systemd/system/vault.service.d
+rm -rf /opt/vault
 
