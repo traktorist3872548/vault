c22a4b37a430fabbf78d746e5c5242f636b4920f
traktorist3872548 <redisav@gmail.com>
2019-08-27 18:43:06 +0300

    Delete unseal.sh

--- unseal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-export VAULT_ADDR='http://0.0.0.0:8200'
-egrep -m3 '^Unseal Key' /opt/vault/init.file | cut -f2- -d: | tr -d ' ' | while read key; do   /opt/vault/vault operator unseal ${key}; done
-
