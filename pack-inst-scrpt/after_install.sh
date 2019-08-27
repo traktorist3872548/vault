bf6ad29507b69ca64015ae0c89c8b81f419e189c
traktorist3872548 <redisav@gmail.com>
2019-08-27 19:35:39 +0300

    Update after_install.sh

--- pack-inst-scrpt/after_install.sh
+++ pack-inst-scrpt/after_install.sh
@@ -1,8 +1,4 @@
 
 #!/bin/bash -x
 export VAULT_ADDR='http://127.0.0.1:8200'
-nohup /opt/vault/vault server -dev -config=/opt/vault/vault.hcl &
 export VAULT_TOKEN=0000-0000-0000-0000
-sleep 2
-/opt/vault/setup_vault_data.sh
-pkill vault
