1d3230ffd483d9c0c1e434ace3187a52420d979c
traktorist3872548 <redisav@gmail.com>
2019-08-27 20:10:27 +0300

    Update after_install.sh

--- pack-inst-scrpt/after_install.sh
+++ pack-inst-scrpt/after_install.sh
@@ -2,5 +2,5 @@
 #!/bin/bash -x
 export VAULT_ADDR='http://127.0.0.1:8200'
 export VAULT_TOKEN=0000-0000-0000-0000
-chown -R vault:vault /opt/vault
-chmod -R 750 /opt/vault
+chown -R vault:vault {/opt/vault,/usr/lib/systemd/system/vault.service} 
+chmod -R 750 {/opt/vault,/usr/lib/systemd/system/vault.service}
